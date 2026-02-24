aa4d60d9032c2d924985f8a1378bf86ed20919e7
LorenzoLX <88005372+LorenzoLX@users.noreply.github.com>
2026-02-24 14:22:56 -0300

    Refactor LevelSearchLayer keybinds implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/LevelSearchLayer.hpp>
-//#include <geode.custom-keybinds/include/Keybinds.hpp>
-
+#include <geode.custom-keybinds/include/Keybinds.hpp>
 using namespace geode::prelude;
+using namespace keybinds;
 
 $execute {
-  using namespace keybinds;
-
   BindManager::get()->registerBindable({
     "enter-search"_spr,
     "Enter Search",
@@ -16,22 +14,18 @@ $execute {
   });
 }
 
-class $modify(LevelSearchLayer){
-  bool init(int p0){
+class $modify(LevelSearchLayer) {
+  bool init(int p0) {
     if (!LevelSearchLayer::init(p0)) {
       return false;
     }
-
-    this->template addEventListener<keybinds::InvokeBindFilter>([=](keybinds::InvokeBindEvent* event) {
+    this->template addEventListener<InvokeBindFilter>([=, this](InvokeBindEvent* event) {
       auto levelSearch = CCDirector::get()->getRunningScene()->getChildByID("LevelSearchLayer");
-
       if (event->isDown() && levelSearch != nullptr) {
         this->onSearch(nullptr);
       }
       return ListenerResult::Propagate;
     }, "enter-search"_spr);
-
     return true;
   }
 };
-
